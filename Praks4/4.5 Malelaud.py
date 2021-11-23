9c550d5bc9164893a198597df073d503b6a4b5cc
Enar <enar.joesaar@khk.ee>
2021-11-23 10:54:17 +0200

    4.5 Malelaud.py

---
+++ Praks4/4.5 Malelaud.py
@@ -0,0 +1,135 @@
+from tkinter import *
+ 
+raam = Tk()
+raam.title("Malelaud")
+tahvel = Canvas(raam, width=800, height = 800)
+#esimene
+tahvel.create_rectangle(0, 0, 100, 100, fill="black", outline="black")
+
+tahvel.create_rectangle(0, 100, 100, 200, fill="white", outline="white")
+
+tahvel.create_rectangle(0, 200, 100, 300, fill="black", outline="black")
+
+tahvel.create_rectangle(0, 300, 100, 400, fill="white", outline="white")
+
+tahvel.create_rectangle(0, 400, 100, 500, fill="black", outline="black")
+
+tahvel.create_rectangle(0, 500, 100, 600, fill="white", outline="white")
+
+tahvel.create_rectangle(0, 600, 100, 700, fill="black", outline="black")
+
+tahvel.create_rectangle(0, 700, 100, 800, fill="white", outline="white")
+#teine
+tahvel.create_rectangle(200, 0, 100, 100, fill="white", outline="white")
+
+tahvel.create_rectangle(200, 100, 100, 200, fill="black", outline="black")
+
+tahvel.create_rectangle(200, 200, 100, 300, fill="white", outline="white")
+
+tahvel.create_rectangle(200, 300, 100, 400, fill="black", outline="black")
+
+tahvel.create_rectangle(200, 400, 100, 500, fill="white", outline="white")
+
+tahvel.create_rectangle(200, 500, 100, 600, fill="black", outline="black")
+
+tahvel.create_rectangle(200, 600, 100, 700, fill="white", outline="white")
+
+tahvel.create_rectangle(200, 700, 100, 800, fill="black", outline="black")
+#kolmas
+tahvel.create_rectangle(300, 0, 200, 100, fill="black", outline="black")
+
+tahvel.create_rectangle(300, 100, 200, 200, fill="white", outline="white")
+
+tahvel.create_rectangle(300, 200, 200, 300, fill="black", outline="black")
+
+tahvel.create_rectangle(300, 300, 200, 400, fill="white", outline="white")
+
+tahvel.create_rectangle(300, 400, 200, 500, fill="black", outline="black")
+
+tahvel.create_rectangle(300, 500, 200, 600, fill="white", outline="white")
+
+tahvel.create_rectangle(300, 600, 200, 700, fill="black", outline="black")
+
+tahvel.create_rectangle(300, 700, 200, 800, fill="white", outline="white")
+#neljas
+tahvel.create_rectangle(400, 0, 300, 100, fill="white", outline="white")
+
+tahvel.create_rectangle(400, 100, 300, 200, fill="black", outline="black")
+
+tahvel.create_rectangle(400, 200, 300, 300, fill="white", outline="white")
+
+tahvel.create_rectangle(400, 300, 300, 400, fill="black", outline="black")
+
+tahvel.create_rectangle(400, 400, 300, 500, fill="white", outline="white")
+
+tahvel.create_rectangle(400, 500, 300, 600, fill="black", outline="black")
+
+tahvel.create_rectangle(400, 600, 300, 700, fill="white", outline="white")
+
+tahvel.create_rectangle(400, 700, 300, 800, fill="black", outline="black")
+#viies
+tahvel.create_rectangle(500, 0, 400, 100, fill="black", outline="black")
+
+tahvel.create_rectangle(500, 100, 400, 200, fill="white", outline="white")
+
+tahvel.create_rectangle(500, 200, 400, 300, fill="black", outline="black")
+
+tahvel.create_rectangle(500, 300, 400, 400, fill="white", outline="white")
+
+tahvel.create_rectangle(500, 400, 400, 500, fill="black", outline="black")
+
+tahvel.create_rectangle(500, 500, 400, 600, fill="white", outline="white")
+
+tahvel.create_rectangle(500, 600, 400, 700, fill="black", outline="black")
+
+tahvel.create_rectangle(500, 700, 400, 800, fill="white", outline="white")
+#kuues
+tahvel.create_rectangle(600, 0, 500, 100, fill="white", outline="white")
+
+tahvel.create_rectangle(600, 100, 500, 200, fill="black", outline="black")
+
+tahvel.create_rectangle(600, 200, 500, 300, fill="white", outline="white")
+
+tahvel.create_rectangle(600, 300, 500, 400, fill="black", outline="black")
+
+tahvel.create_rectangle(600, 400, 500, 500, fill="white", outline="white")
+
+tahvel.create_rectangle(600, 500, 500, 600, fill="black", outline="black")
+
+tahvel.create_rectangle(600, 600, 500, 700, fill="white", outline="white")
+
+tahvel.create_rectangle(600, 700, 500, 800, fill="black", outline="black")
+#seitsmes
+tahvel.create_rectangle(700, 0, 600, 100, fill="black", outline="black")
+
+tahvel.create_rectangle(700, 100, 600, 200, fill="white", outline="white")
+
+tahvel.create_rectangle(700, 200, 600, 300, fill="black", outline="black")
+
+tahvel.create_rectangle(700, 300, 600, 400, fill="white", outline="white")
+
+tahvel.create_rectangle(700, 400, 600, 500, fill="black", outline="black")
+
+tahvel.create_rectangle(700, 500, 600, 600, fill="white", outline="white")
+
+tahvel.create_rectangle(700, 600, 600, 700, fill="black", outline="black")
+
+tahvel.create_rectangle(700, 700, 600, 800, fill="white", outline="white")
+#kaheksas
+tahvel.create_rectangle(800, 0, 700, 100, fill="white", outline="white")
+
+tahvel.create_rectangle(800, 100, 700, 200, fill="black", outline="black")
+
+tahvel.create_rectangle(800, 200, 700, 300, fill="white", outline="white")
+
+tahvel.create_rectangle(800, 300, 700, 400, fill="black", outline="black")
+
+tahvel.create_rectangle(800, 400, 700, 500, fill="white", outline="white")
+
+tahvel.create_rectangle(800, 500, 700, 600, fill="black", outline="black")
+
+tahvel.create_rectangle(800, 600, 700, 700, fill="white", outline="white")
+
+tahvel.create_rectangle(800, 700, 700, 800, fill="black", outline="black")
+tahvel.pack()
+raam.mainloop()
